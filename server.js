ff649db1236892e08aef856811ad48f814c8088a
Kathleen Tonner <kathleentonner@gmail.com>
2020-05-04 19:29:17 -0400

    come the fuck on

--- server.js
+++ server.js
@@ -15,9 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines", { useNewUrlParser: true, useUnifiedTopology: true  });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_kp250xt2:f30n6ggsji2gjkhij2dejkui0a@ds163698.mlab.com:63698/heroku_kp250xt2", { useNewUrlParser: true, useUnifiedTopology: true  });
 
 // routes
 app.use(require("./routes/api.js"));
