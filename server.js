e633e7dc4fa8909428d2bfbc58bb7c4ebd45a28d
Kathleen Tonner <kathleentonner@gmail.com>
2020-05-04 15:59:46 -0400

    mongoose connection

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines", { useNewUrlParser: true, useUnifiedTopology: true  });
 
 // routes
 app.use(require("./routes/api.js"));
